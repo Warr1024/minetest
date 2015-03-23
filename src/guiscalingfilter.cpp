6a5c8dcd292335d79fc868ae34fac20b016e1595
Aaron Suen <warr1024@gmail.com>
2015-03-22 22:29:48 -0400

    Cache generated textures, too, and clear on new login. This ensures that connecting to different servers (or different restarts of the same server) in the same session gives non-stale textures, in case textures with the same name changed.  Also frees up some memory when server-hopping.

--- src/guiscalingfilter.cpp
+++ src/guiscalingfilter.cpp
@@ -11,6 +11,11 @@
  */
 std::map<io::path, video::IImage *> imgCache;
 
+/* Maintain a static cache of all pre-scaled textures.  These need to be
+ * cleared as well when the cached images.
+ */
+std::map<io::path, video::ITexture *> txrCache;
+
 /* Manually insert an image into the cache, useful to avoid texture-to-image
  * conversion whenever we can intercept it.
  */
@@ -20,10 +25,28 @@ void guiScalingCache(io::path key, video::IVideoDriver *driver, video::IImage *v
 	video::IImage *copied = driver->createImage(value->getColorFormat(),
 			value->getDimension());
 	value->copyTo(copied);
-	copied->grab();
 	imgCache[key] = copied;
 }
 
+// Manually clear the cache, e.g. when switching to different worlds.
+void guiScalingCacheClear(video::IVideoDriver *driver) {
+	for(std::map<io::path, video::IImage *>::iterator it = imgCache.begin();
+			it != imgCache.end(); it++) {
+		if (it->second != NULL)
+			it->second->drop();
+	}
+	imgCache.clear();
+	for(std::map<io::path, video::ITexture *>::iterator it = txrCache.begin();
+			it != txrCache.end(); it++) {
+		if (it->second != NULL)
+			driver->removeTexture(it->second);
+	}
+	txrCache.clear();
+}
+
+#ifdef __ANDROID__
+// Compute next-higher power of 2 efficiently, for platforms that
+// require power-of-2 textures.
 u32 nextpower2(u32 orig) {
 	orig--;
 	orig |= orig >> 1;
@@ -33,7 +56,12 @@ u32 nextpower2(u32 orig) {
 	orig |= orig >> 16;
 	return orig + 1;
 }
+#endif
 
+/* Get a cached, high-quality pre-scaled texture for display purposes.  If the
+ * texture is not already cached, attempt to create it.  Returns a pre-scaled texture,
+ * or the original texture if unable to pre-scale it.
+ */
 video::ITexture *guiScalingResizeCached(video::IVideoDriver *driver, video::ITexture *src,
 		const core::rect<s32> &srcrect, const core::rect<s32> &destrect) {
 
@@ -53,7 +81,7 @@ video::ITexture *guiScalingResizeCached(video::IVideoDriver *driver, video::ITex
 	io::path scalename = origname + "@guiScalingFilter:" + rectstr;
 
 	// Search for existing scaled texture.
-	video::ITexture *scaled = driver->getTexture(scalename);
+	video::ITexture *scaled = txrCache[scalename];
 	if(scaled)
 		return scaled;
 
@@ -76,10 +104,10 @@ video::ITexture *guiScalingResizeCached(video::IVideoDriver *driver, video::ITex
 			(u32)destrect.getHeight()));
 	imageScaleNNAA(srcimg, srcrect, destimg);
 
+#ifdef __ANDROID__
 	// Android is very picky about textures being powers of 2, so expand
 	// the image dimensions to the next power of 2, if necessary, for
 	// that platform.
-#ifdef __ANDROID__
 	video::IImage *po2img = driver->createImage(src->getColorFormat(),
 			core::dimension2d<u32>(nextpower2((u32)destrect.getWidth()),
 			nextpower2((u32)destrect.getHeight())));
@@ -91,10 +119,14 @@ video::ITexture *guiScalingResizeCached(video::IVideoDriver *driver, video::ITex
 	// Convert the scaled image back into a texture.
 	scaled = driver->addTexture(scalename, destimg, NULL);
 	destimg->drop();
+	txrCache[scalename] = scaled;
 
 	return scaled;
 }
 
+/* Convenience wrapper for guiScalingResizeCached that accepts parameters that
+ * are available at GUI imagebutton creation time.
+ */
 video::ITexture *guiScalingImageButton(video::IVideoDriver *driver, video::ITexture *src,
 		s32 width, s32 height) {
 	return guiScalingResizeCached(driver, src,
@@ -102,10 +134,13 @@ video::ITexture *guiScalingImageButton(video::IVideoDriver *driver, video::IText
 		core::rect<s32>(0, 0, width, height));
 }
 
+/* Replacement for driver->draw2DImage() that uses the high-quality pre-scaled
+ * texture, if configured.
+ */
 void draw2DImageFilterScaled(video::IVideoDriver *driver, video::ITexture *txr,
 		const core::rect<s32> &destrect, const core::rect<s32> &srcrect,
-		const core::rect<s32> *cliprect = 0, const video::SColor *const colors = 0,
-		bool usealpha = false) {
+		const core::rect<s32> *cliprect, const video::SColor *const colors,
+		bool usealpha) {
 
 	// Attempt to pre-scale image in software in high quality.
 	video::ITexture *scaled = guiScalingResizeCached(driver, txr, srcrect, destrect);
--- src/guiscalingfilter.h
+++ src/guiscalingfilter.h
@@ -8,15 +8,28 @@
  */
 void guiScalingCache(io::path key, video::IVideoDriver *driver, video::IImage *value);
 
-video::ITexture *guiScalingResizeCached(video::IVideoDriver *, video::ITexture *,
-		const core::rect<s32> &, const core::rect<s32> &);
+// Manually clear the cache, e.g. when switching to different worlds.
+void guiScalingCacheClear(video::IVideoDriver *driver);
 
+/* Get a cached, high-quality pre-scaled texture for display purposes.  If the
+ * texture is not already cached, attempt to create it.  Returns a pre-scaled texture,
+ * or the original texture if unable to pre-scale it.
+ */
+video::ITexture *guiScalingResizeCached(video::IVideoDriver *driver, video::ITexture *src,
+		const core::rect<s32> &srcrect, const core::rect<s32> &destrect);
+
+/* Convenience wrapper for guiScalingResizeCached that accepts parameters that
+ * are available at GUI imagebutton creation time.
+ */
 video::ITexture *guiScalingImageButton(video::IVideoDriver *driver, video::ITexture *src,
-	s32 width, s32 height);
+		s32 width, s32 height);
 
+/* Replacement for driver->draw2DImage() that uses the high-quality pre-scaled
+ * texture, if configured.
+ */
 void draw2DImageFilterScaled(video::IVideoDriver *driver, video::ITexture *txr,
 		const core::rect<s32> &destrect, const core::rect<s32> &srcrect,
-		const core::rect<s32> *cliprect, const video::SColor *const colors,
-		bool usealpha);
+		const core::rect<s32> *cliprect = 0, const video::SColor *const colors = 0,
+		bool usealpha = false);
 
 #endif
